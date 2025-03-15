c1a354272aa200e229a53b5226a6d8e399ff0389
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-03-15 23:22:58 +0000

    exercicio 3 funcionando par algums entradas, falt verificar mais de um padrao

--- exercicio3.c
+++ exercicio3.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-    char str[100] = "ababababababababababab";
+     char str[100];
     char strDoPadrao[20];
-    int contador = 1;
+    int contador = 0;
 
     //pegando a entrada do usuario
-    //printf("Digite um numero: ");
-    //scanf("%99s", str);
+    printf("Digite a entrada: ");
+    scanf("%99s", str);
 
-    //pegando a primeira palavra
-    char letra = str[1];
+    //pegando a primeira letra
+    char letra = str[0];
+
+    //pegndo o tamanho da string
+    int tamanho = strlen(str);
 
     //percorrendo a string e pegando o numer de rep
-    for (int i = 0; i < strlen(str); i++) {
+    for (int i = 0; i < tamanho; i++) {
         if (str[i] == letra) {
             contador ++;
         }
     }
 
-    //pegando o padrao
-    for (int i = 0; i < strlen(str); i++) {
-        if (letra != str[i]) {
-            strDoPadrao += str[i];
+    // j ser usado para adicionar o padrao
+    int j = 1;
+    strDoPadrao[0] = letra;
+
+    for (int i = 1; i < tamanho; i++) { 
+        if (str[i] != letra) {
+            strDoPadrao[j++] = str[i];
         } else {
-            break;
+            break; // Quando encontrar a letra igual a primeira, sai do laco
         }
     }
+    strDoPadrao[j] = '\0'; // Finaliza a string
 
     // printando o padrao e a qtd de vezes que se repetiu
-    printf("%s%d", strDoPadrao, contador);   
+    printf(">'");
+    printf("%s%d\n", strDoPadrao, contador);
+    printf("'");
+
+    return 0;
+
 }
