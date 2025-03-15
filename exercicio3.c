ae29994df7318219d3919373680a00d469591134
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-03-15 22:32:19 +0000

    tentando fazer o exercicio 3

--- exercicio3.c
+++ exercicio3.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
 int main(void) {
-    char str[100];
-    char strDoPadrao[3];
+    char str[100] = "ababababababababababab";
+    char strDoPadrao[20];
+    int contador = 1;
 
     //pegando a entrada do usuario
-    printf("Digite um numero: ");
-    scanf("%99s", str);
+    //printf("Digite um numero: ");
+    //scanf("%99s", str);
 
-    //identificando o padrao que a entrada segue
-    for (int i=0; i < strlen(str); i++) {
-        
+    //pegando a primeira palavra
+    char letra = str[1];
+
+    //percorrendo a string e pegando o numer de rep
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == letra) {
+            contador ++;
+        }
     }
+
+    //pegando o padrao
+    for (int i = 0; i < strlen(str); i++) {
+        if (letra != str[i]) {
+            strDoPadrao += str[i];
+        } else {
+            break;
+        }
+    }
+
+    // printando o padrao e a qtd de vezes que se repetiu
+    printf("%s%d", strDoPadrao, contador);   
 }
