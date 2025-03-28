d21b90de3499adb969730811689f20f51220d643
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-03-28 14:58:25 -0300

    Update ex2.c
    solucao otima exercicio 2

--- ex2.c
+++ ex2.c
@@ -10,31 +10,23 @@ int main(void) {
     printf("Informe a string a ser descompactada: ");
     scanf("%199s", entrada);
 
-    char *ptr_inicio = entrada;
-    char *ptr_final = entrada;
+    char *ptr_inicio, *ptr_final = entrada;
     char num_rep_concatenado[50];
-    int i = 0;
-    int cont = 0;
+    int i, cont = 0;
 
     while (*ptr_final != '\0') {
         if (isalpha(*ptr_final)) 
         {
             char letra = *ptr_final;
             ptr_final++; 
-            i = 0;  
+            int repeticoes = 0; 
 
-            while (*ptr_final != '-' && *ptr_final != '\0') 
-            {
-                num_rep_concatenado[i] = *ptr_final;
+            while (isdigit(*ptr_final)) {
+                repeticoes = repeticoes * 10 + (*ptr_final - '0');
                 ptr_final++;
-                i++;
             }
 
-            num_rep_concatenado[i] = '\0';
-
-            int valor_rep = atoi(num_rep_concatenado);
-
-            for (int j = 0; j < valor_rep; j++) printf("%c", letra);
+            for (int j = 0; j < repeticoes; j++) printf("%c", letra);
 
             if (*ptr_final == '-') {
                 ptr_final++;  
