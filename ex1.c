45d0c4dc37f62934c5c64f1a5cfd816e09de6dd4
Ryan <ledoryan42@gmail.com>
2025-03-28 01:20:53 -0300

    Melhorei o ex1 e o ex2

---
+++ ex1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct {
+    char letra;
+    int numero; 
+} Compactado;
+
+
+int main(void) {
+    char entrada[100] = "abcabc";
+    int tamanho = strlen(entrada);
+    char *ptr_inicio = entrada;
+    char *ptr_final = entrada;
+
+    int contador = 1;
+
+    while (*ptr_final != '\0') {
+        ptr_final++;
+
+        if (*ptr_inicio == *ptr_final)
+        {
+            contador++;
+        }
+        else
+        {
+            Compactado s;
+
+            s.letra = *ptr_inicio;
+            s.numero = contador;
+
+            if (*ptr_final == entrada[tamanho]) printf("%c%d", s.letra, s.numero);
+            else  printf("%c%d-", s.letra, s.numero);
+
+            contador = 1;
+        }
+        ptr_inicio = ptr_final;
+    }
+    printf("\n");
+}
