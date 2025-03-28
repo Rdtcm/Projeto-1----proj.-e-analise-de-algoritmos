6610c2ad2f8280b52661ed5e027d1261ad0dafa0
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-03-28 14:51:05 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+#define MAX 10000
 
 int main(void) {
-    char entrada[100] = "a7-b5-a10";
+    char entrada[MAX];
+    
+    printf("Informe a string a ser descompactada: ");
+    scanf("%199s", entrada);
+
     char *ptr_inicio = entrada;
-    char *ptr_final = entrada + 1;
+    char *ptr_final = entrada;
+    char num_rep_concatenado[50];
+    int i = 0;
+    int cont = 0;
 
-    while (*ptr_final != '\0') 
-    {
-        int valor_ptr_final = atoi(ptr_final);
+    while (*ptr_final != '\0') {
+        if (isalpha(*ptr_final)) 
+        {
+            char letra = *ptr_final;
+            ptr_final++; 
+            i = 0;  
 
-        for (int i = 0; i < valor_ptr_final; i++) printf("%c", *ptr_inicio);
-        
-        ptr_inicio += 3;
-        ptr_final += 3;
+            while (*ptr_final != '-' && *ptr_final != '\0') 
+            {
+                num_rep_concatenado[i] = *ptr_final;
+                ptr_final++;
+                i++;
+            }
+
+            num_rep_concatenado[i] = '\0';
+
+            int valor_rep = atoi(num_rep_concatenado);
+
+            for (int j = 0; j < valor_rep; j++) printf("%c", letra);
+
+            if (*ptr_final == '-') {
+                ptr_final++;  
+            }
+        } 
+        else 
+        {
+            ptr_final++;
+        }
     }
     printf("\n");
+    return 0;
 }
